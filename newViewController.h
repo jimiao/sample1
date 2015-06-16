98cbdbae136a7c66fd4c9346bbbda25c4fe9056e
jimiao <jimiao@hz.totyu.com>
2015-06-16 13:16:24 +0800

    the first

---
+++ newViewController.h
@@ -0,0 +1,13 @@
+//
+//  newViewController.h
+//  gitsample2
+//
+//  Created by totyu2 on 2015/06/16.
+//  Copyright (c) 2015年 totyu2. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface newViewController : NSViewController
+
+@end
---
+++ newViewController.m
@@ -0,0 +1,22 @@
+//
+//  newViewController.m
+//  gitsample2
+//
+//  Created by totyu2 on 2015/06/16.
+//  Copyright (c) 2015年 totyu2. All rights reserved.
+//
+
+#import "newViewController.h"
+
+@interface newViewController ()
+
+@end
+
+@implementation newViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
